5997fb5e801bc748e04e266889eda179492255a4
Brandon <bcharris713@gmail.com>
2022-05-05 12:59:21 -0500

    commit tree b38cde3c9bfdbd3e3298d83ec7ab4055863a2c50

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+
+let num3 = 19;
+let num4 = 5;
+
+let multiply = num1 * num2;
+
+let random;
+random = Math.floor(Math.random() * 1) + 1;
+
+let mod;
+mod = num3 % num4;
+
+let max;
+max = Math.max(1, 2, 3, 4, 20);
